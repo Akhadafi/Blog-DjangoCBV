91a1b37e3d95a06baf3bff70956dd38dca0dd68f
Akhadafi <khadafi.dev@gmail.com>
2022-10-15 21:40:27 +0700

    list view

--- artikel/urls.py
+++ artikel/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from artikel.views import ArtikelListView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="main.html"), name="home"),
+    path("artikel/", ArtikelListView.as_view(), name="list"),
 ]
--- artikel/views.py
+++ artikel/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from artikel.models import Artikel
 
-# Create your views here.
+
+class ArtikelListView(ListView):
+    model = Artikel
+    template_name = "artikel/list.html"
+    context_object_name = "artikel_list"
