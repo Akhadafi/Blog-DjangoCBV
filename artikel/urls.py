052644af878a4cc5f57d5f43e8bfd7c95d25d66e
Akhadafi <khadafi.dev@gmail.com>
2022-10-15 22:17:46 +0700

    detail view

--- artikel/urls.py
+++ artikel/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from artikel.views import ArtikelListView
+from artikel.views import ArtikelListView, ArtikelDetailView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="main.html"), name="home"),
     path("artikel/", ArtikelListView.as_view(), name="list"),
+    path("artikel/<slug:slug>", ArtikelDetailView.as_view(), name="detail"),
 ]
--- artikel/views.py
+++ artikel/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from artikel.models import Artikel
 
 
@@ -6,3 +6,9 @@ class ArtikelListView(ListView):
     model = Artikel
     template_name = "artikel/list.html"
     context_object_name = "artikel_list"
+
+
+class ArtikelDetailView(DetailView):
+    model = Artikel
+    template_name = "artikel/detail.html"
+    context_object_name = "artikel"
