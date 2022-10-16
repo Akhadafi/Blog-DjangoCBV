4ac2eccf85d283303d8cae0dc9cf23c2ab1e0350
Akhadafi <khadafi.dev@gmail.com>
2022-10-16 11:37:43 +0700

    pagination

--- artikel/urls.py
+++ artikel/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 from artikel.views import ArtikelListView, ArtikelDetailView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="main.html"), name="home"),
-    path("artikel/", ArtikelListView.as_view(), name="list"),
+    re_path(r"^artikel/(?P<page>\d+)$", ArtikelListView.as_view(), name="list"),
     path("artikel/<slug:slug>", ArtikelDetailView.as_view(), name="detail"),
 ]
--- artikel/views.py
+++ artikel/views.py
@@ -6,6 +6,8 @@ class ArtikelListView(ListView):
     model = Artikel
     template_name = "artikel/list.html"
     context_object_name = "artikel_list"
+    ordering = ["-publish"]
+    paginate_by = 3
 
 
 class ArtikelDetailView(DetailView):
